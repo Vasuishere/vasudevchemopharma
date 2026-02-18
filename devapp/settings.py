cf986668bf60fc2ae3b6e4ba0b06d813cc525b75
vasudevchemopharma-gif <vasudevchemopharma@gmail.com>
2026-02-18 13:56:03 +0530

    Refactor database settings to use dj-database-url for configuration

--- devapp/settings.py
+++ devapp/settings.py
@@ -76,17 +76,17 @@ TEMPLATES = [
 WSGI_APPLICATION = 'devapp.wsgi.application'
 
 
+import os
+import dj_database_url
+
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}'
+    )
 }
-
-import os
 
 STATIC_URL = '/static/'
 
